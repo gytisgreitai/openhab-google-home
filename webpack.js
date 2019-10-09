3e59b3dcd713b077aa72754771895083790eb018
gytisgreitai <gytis@fmu1.net>
2019-10-09 19:18:51 +0300

    add webpack to produce single runnable file

---
+++ webpack.js
@@ -0,0 +1,32 @@
+const path = require('path');
+module.exports =  {
+    entry: './src/index.ts',
+    target: "node",
+    devtool: 'none',
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'index-nodeps.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+        modules: [
+            path.resolve(__dirname, 'node_modules')
+        ],
+    },
+    optimization: {
+        minimize: false,
+        nodeEnv: false
+    },
+    module: {
+        rules: [
+        {
+            use: 'ts-loader',
+            test: /\.ts?$/
+        }
+        ]
+    },
+    node: {
+        __dirname: false
+    }
+}
